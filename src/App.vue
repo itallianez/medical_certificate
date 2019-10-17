81ee85fa8f40222d75111cd6471bb2aa8b5d5909
LeonardoShubin <leonardoshubin@gmail.com>
2019-10-17 13:55:10 +0300

    test roots

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      Hi, I can change it
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
